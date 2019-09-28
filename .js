09f053a3e09516095e118705e914d2e3b2fe7098
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-09-27 22:18:48 -0300

    Create .js

---
+++ .js
@@ -0,0 +1,12 @@
+// in contruction!
+// use this script for a chat
+  let name = prompt("What is your name?");
+  alert("Hello, " + name + "!");
+  let humor = prompt("you are happy?");
+  alert("ok!")
+  let plan = prompt("what you plan to do?")
+  alert("cool!")
+  let idea1 = prompt("why do not you see some videos on youtube?, " + name + "?")
+  alert("ok, ;-; 👌")
+  alert("hmmm...")
+  alert("how about you sing a song?" + name + "?")
