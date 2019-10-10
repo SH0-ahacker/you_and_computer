e76b3d8fd363df12b846c665c52cf5c6c582f23d
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-10-09 21:59:27 -0300

    Update .js

--- .js
+++ .js
@@ -1,6 +1,25 @@
 // in contruction!, this note is a info!, this is a script-test, and not a "perfect-script"!
-// use this script for a chat
-  let name = prompt("What is your name?");
+// use this script for a chat in you and your computer......................................
+<script>
+// this is the configuration, don't delete this part, because, part deleted = co not run this game!!!!!!!!!!!!!!!
+document.cookie = "user=human";
+document.cookie = "internet=ok";
+document.cookie = "#=#";
+var day     = data.getDate();
+var day2    = data.getDay();
+var month     = data.getMonth(); 
+var year    = data.getYear(); 
+var fullyear    = data.getFullYear();  
+var hours    = data.getHours();
+var min     = data.getMinutes();
+var seg     = data.getSeconds();
+var mseg    = data.getMilliseconds();
+var tz      = data.getTimezoneOffset();
+
+</script>
+<script>
+alert("FULL RECOMENDATION:play a cool music for the best expirience of your life!")
+let name = prompt("What is your name?");
   alert("Hello, " + name + "!");
   let humor = prompt("you are happy?");
   alert("ok!")
@@ -11,7 +30,7 @@
   alert("hmmm...")
   alert("how about you sing a song? " + name + "?")
   let game = prompt("why do not you play a game " + name + "?")
-  let date = prompt("the date is:")
-  alert("ok! '-'")
-  let timeHOUR = prompt("HOUR'S, " + name + "?")
-  alert("thank u! ")
+alert("I know what time it is, the time is:" + Date() + name + "!")  
+  
+
+</script>
